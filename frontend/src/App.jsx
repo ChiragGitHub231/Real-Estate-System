063504c4374600a602a99c773168b996000a6b78
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2023-12-17 16:04:38 +0530

    Router Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import Privacy from './pages/Privacy';
+import ContactUs from './pages/ContactUs';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/privacy' element={<Privacy />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
