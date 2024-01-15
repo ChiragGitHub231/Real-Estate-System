e380c10818af01372a878a152d45b0c67225c642
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-01-15 15:55:43 +0530

    Create Listing API Route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,11 +6,6 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    // contactno:{
-    //     type: String,
-    //     required: true,
-    //     unique: true,
-    // },
     email: {
         type: String,
         required: true,
